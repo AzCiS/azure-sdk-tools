b2459aad1602648487545fc53edc3bd7f3995948
parvezah <parvezah@microsoft.com>
2014-08-27 11:28:05 +0530

    Base Powershell structure.

---
+++ src/ServiceManagement/StorSimple/Commands.StorSimple/Cmdlets/GetAzureHCSDevice.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Management.Automation;
+using Microsoft.WindowsAzure;
+
+namespace Micro.Azure.Commands.StorSimple.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "AzureHCSDevice")]
+    public class GetAzureHCSDevice : StorSimpleCmdletBase
+    {
+        public override void ExecuteCmdlet()
+        {
+            try
+            {
+                var deviceInfos = StorSimpleClient.GetAllDevices();
+                foreach (var deviceInfo in deviceInfos)
+                {
+                    this.WriteObject(deviceInfo);
+                }
+            }
+            catch (CloudException cloudException)
+            {
+                StorSimpleClient.ThrowCloudExceptionDetails(cloudException);
+            }
+        }
+    }
+}
