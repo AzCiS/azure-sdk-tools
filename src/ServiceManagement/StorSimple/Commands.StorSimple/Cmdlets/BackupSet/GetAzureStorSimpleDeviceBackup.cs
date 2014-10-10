67bd0ec157131c936aa9cd3e44bc128b61b4b8cf
avirupch <avirupc@outlook.com>
2014-10-10 11:00:07 +0530

    adding backupset related files

---
+++ src/ServiceManagement/StorSimple/Commands.StorSimple/Cmdlets/BackupSet/GetAzureStorSimpleDeviceBackup.cs
@@ -0,0 +1,175 @@
+﻿using System.Linq;
+using System.Management.Automation;
+using Microsoft.WindowsAzure.Management.StorSimple.Models;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Commands.Utilities.CloudService;
+
+namespace Microsoft.WindowsAzure.Commands.StorSimple.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "AzureStorSimpleDeviceBackup"),OutputType(typeof(GetBackupResponse))]
+    public class GetAzureStorSimpleDeviceBackup: StorSimpleCmdletBase
+    {
+        private const string PARAMETERSET_DEFAULT = "Default";
+        private const string PARAMETERSET_BACKUPPOLICYIDFILTER = "BackupPolicyIdFilter";
+        private const string PARAMETERSET_VOLUMEIDFILTER = "VolumeIdFilter";
+        private const string PARAMETERSET_BACKUPPOLICYOBJECTFILTER = "BackupPolicyObjectFilter";
+        private const string PARAMETERSET_VOLUMEOBJECTFILTER = "VolumeObjectFilter";
+
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_DEFAULT)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_BACKUPPOLICYIDFILTER)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_VOLUMEIDFILTER)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_BACKUPPOLICYOBJECTFILTER)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_VOLUMEOBJECTFILTER)]
+        [ValidateNotNullOrEmpty]
+        public string DeviceName { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "InstanceId of the backupPolicy which created the backups.", ParameterSetName = PARAMETERSET_BACKUPPOLICYIDFILTER)]
+        public Guid? BackupPolicyId { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "InstanceId of the volume in which backups exist.", ParameterSetName = PARAMETERSET_VOLUMEIDFILTER)]
+        public String VolumeId { get; set; }
+
+        [Alias("BackupPolicyDetails")]
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Provide the BackupPolicyDetails object. The InstanceId of this object will be used as a filter for backups.", ParameterSetName = PARAMETERSET_BACKUPPOLICYOBJECTFILTER)]
+        public BackupPolicyDetails BackupPolicy { get; set; }
+
+        [Alias("VirtualDiskInfo")]
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Provide the VirtualDisk object. The InstanceId of this object will be used as a filter for backups.", ParameterSetName = PARAMETERSET_VOLUMEOBJECTFILTER)]
+        public VirtualDisk Volume { get; set; }
+
+        [Parameter(Position = 2, Mandatory = false, HelpMessage = "The start time for filtering backups.")]
+        public DateTime? From { get; set; }
+
+        [Parameter(Position = 3, Mandatory = false, HelpMessage = "The end time for filtering backups.")]
+        public DateTime? To { get; set; }
+
+        [Parameter(Position = 4, Mandatory = false, HelpMessage = "Gets only the specified number of objects. Enter the number of objects to get.")]
+        public int? First { get; set; }
+
+        [Parameter(Position = 5, Mandatory = false, HelpMessage = "Ignores the specified number of objects and then gets the remaining objects. Enter the number of objects to skip.")]
+        public int? Skip { get; set; }
+
+        private string deviceId = null;
+ 
+        public override void ExecuteCmdlet()
+        {
+            try
+            {
+                ProcessParameters();
+                GetBackupResponse backupList = null;
+                switch (ParameterSetName)
+                {
+                    case PARAMETERSET_BACKUPPOLICYIDFILTER:
+                        backupList = StorSimpleClient.GetAllBackups(
+                            deviceId,
+                            "BackupPolicy",
+                            Boolean.FalseString,
+                            BackupPolicyId.ToString(),
+                            From.ToString(),
+                            To.ToString(),
+                            (Skip == null ? null : Skip.ToString()),
+                            (First == null ? null : First.ToString()));
+                        break;
+                    case PARAMETERSET_VOLUMEIDFILTER:
+                         backupList = StorSimpleClient.GetAllBackups(
+                            deviceId,
+                            "Volume",
+                            Boolean.FalseString,
+                            VolumeId,
+                            From.ToString(),
+                            To.ToString(),
+                            (Skip == null ? null : Skip.ToString()),
+                            (First == null ? null : First.ToString()));
+                        break;
+                    case PARAMETERSET_BACKUPPOLICYOBJECTFILTER:
+                        backupList = StorSimpleClient.GetAllBackups(
+                            deviceId,
+                            "BackupPolicy",
+                            Boolean.FalseString,
+                            BackupPolicy.InstanceId.ToString(),
+                            From.ToString(),
+                            To.ToString(),
+                            (Skip == null ? null : Skip.ToString()),
+                            (First == null ? null : First.ToString()));
+                        break;
+                    case PARAMETERSET_VOLUMEOBJECTFILTER:
+                        backupList = StorSimpleClient.GetAllBackups(
+                            deviceId,
+                            "Volume",
+                            Boolean.FalseString,
+                            Volume.InstanceId,
+                            From.ToString(),
+                            To.ToString(),
+                            (Skip == null ? null : Skip.ToString()),
+                            (First == null ? null : First.ToString()));
+                        break;
+                    default:
+                            //case where only deviceName is passed
+                        backupList = StorSimpleClient.GetAllBackups(
+                            deviceId,
+                            "BackupPolicy",
+                            Boolean.TrueString,
+                            null,
+                            From.ToString(),
+                            To.ToString(),
+                            (Skip == null ? null : Skip.ToString()),
+                            (First == null ? null : First.ToString()));
+                        break;
+                }
+                WriteObject(backupList, true);
+                WriteVerbose("# of backups returned : " + backupList.BackupSetsList.Count);
+                if (backupList.NextPageUri != null 
+                    && backupList.NextPageStartIdentifier!="1")
+                {
+                    if (First != null)
+                    {
+                        //user has provided First(Top) parameter while calling the commandlet
+                        //so we need to provide it to him for calling the next page
+                        WriteVerbose(String.Format("More backups are available for your query. To access the next page of your result use \"-First {0} -Skip {1}\" in your commandlet", First, backupList.NextPageStartIdentifier));
+                    }
+                    else
+                    {
+                        //user has NOT provided First(Top) parameter while calling the commandlet
+                        //so we DONT need to provide it to him for calling the next page
+                        WriteVerbose(String.Format("More backups are available in the subsequent pages for your query. To access the next page use \"-Skip {0}\"  in your commandlet", backupList.NextPageStartIdentifier));
+                    }
+                }
+                else
+                {
+                    WriteVerbose("No more backup sets are present for your query!");
+                }
+            }
+            catch (CloudException cloudException)
+            {
+                StorSimpleClient.ThrowCloudExceptionDetails(cloudException);
+            }
+        }
+
+        private void ProcessParameters()
+        {
+            var deviceInfos = StorSimpleClient.GetAllDevices(); //get list of all devices
+            foreach (var deviceInfo in deviceInfos)
+            {
+                if (deviceInfo.FriendlyName.Equals(DeviceName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    deviceId = deviceInfo.DeviceId;
+                }
+            }
+            if (deviceId == null)
+                 throw new ArgumentException("Device with name " + DeviceName + "not found");
+            if(First<0)
+                throw new ArgumentException("Parameter First cannot be <0");
+            if (Skip  < 0)
+                throw new ArgumentException("Parameter Skip cannot be <0");
+            if (Skip == null)
+                Skip = 0;
+            if (From == null)
+                From = DateTime.MinValue;
+            if (To == null)
+                To = DateTime.MaxValue;
+        }
+
+    }
+}
---
+++ src/ServiceManagement/StorSimple/Commands.StorSimple/Cmdlets/BackupSet/StartAzureStorSimpleDeviceBackupRestoreJob.cs
@@ -0,0 +1,135 @@
+﻿using System.Linq;
+using System.Management.Automation;
+using Microsoft.WindowsAzure.Management.StorSimple.Models;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Commands.Utilities.CloudService;
+
+namespace Microsoft.WindowsAzure.Commands.StorSimple.Cmdlets
+{
+    using Properties;
+
+    [Cmdlet(VerbsLifecycle.Start, "AzureStorSimpleDeviceBackupRestoreJob")]
+    public class StartAzureStorSimpleDeviceBackupRestoreJob: StorSimpleCmdletBase
+    {
+        private const string PARAMETERSET_BACKUPID = "ByBackupId";
+        private const string PARAMETERSET_BACKUPOBJECT = "ByBackupObject";
+        private const string PARAMETERSET_BACKUPSNAPSHOTID = "ByBackupSnapshotId";
+        private const string PARAMETERSET_BACKUPSNAPSHOTOBJECT = "ByBackupSnapshotObject";
+        String finalBackupId = null;
+        String finalSnapshotId = null;
+        private string deviceId = null;
+
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_BACKUPID)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_BACKUPOBJECT)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_BACKUPSNAPSHOTID)]
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The device name.", ParameterSetName = PARAMETERSET_BACKUPSNAPSHOTOBJECT)]
+        [ValidateNotNullOrEmpty]
+        public string DeviceName { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "InstanceId of the backupSet that needs to be restored", ParameterSetName = PARAMETERSET_BACKUPID)]
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "InstanceId of the backupSet that needs to be restored", ParameterSetName = PARAMETERSET_BACKUPSNAPSHOTID)]
+        public Guid BackupId { get; set; }
+
+        [Parameter(Position = 2, Mandatory = true, HelpMessage = "InstanceId of the Snapshot that needs to be restored", ParameterSetName = PARAMETERSET_BACKUPSNAPSHOTID)]
+        public string SnapshotId { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "BackupSet object which should be used for restore", ParameterSetName = PARAMETERSET_BACKUPOBJECT)]
+        [Parameter(Position = 2, Mandatory = true, HelpMessage = "Snapshot object which should be used for restore", ParameterSetName = PARAMETERSET_BACKUPSNAPSHOTOBJECT)]
+        public Backup Backup { get; set; }
+
+        [Parameter(Position = 2, Mandatory = true, HelpMessage = "Snapshot object which should be used for restore", ParameterSetName = PARAMETERSET_BACKUPSNAPSHOTOBJECT)]
+        public Snapshot Snapshot { get; set; }
+
+        [Alias("WaitForCompletion")]
+        [Parameter(Position = 2, Mandatory = false, HelpMessage = "Wait for remov task complete")]
+        public SwitchParameter WaitForComplete { get; set; }
+
+        [Parameter(HelpMessage = "Force to restore the backup without asking for confirmation")]
+        public SwitchParameter Force
+        {
+            get { return force; }
+            set { force = value; }
+        }
+
+        private bool force;
+
+        public override void ExecuteCmdlet()
+        {
+            try
+            {
+                ProcessParameters();
+                ConfirmAction(
+                   Force.IsPresent,
+                   string.Format(Resources.StartASSDBackupRestoreJobWarningMessage, finalBackupId),
+                   string.Format(Resources.StartASSDBackupRestoreJobMessage, finalBackupId),
+                  finalBackupId,
+                  () =>
+                  {
+                      RestoreBackupRequest request = new RestoreBackupRequest();
+                      request.BackupSetId = finalBackupId;
+                      request.SnapshotId = finalSnapshotId;
+
+                      if (WaitForComplete.IsPresent)
+                      {
+                          var restoreBackupResult = StorSimpleClient.RestoreBackup(deviceId, request);
+                          WriteObject(restoreBackupResult);
+                      }
+                      else
+                      {
+                          //async scenario
+                          var jobresult = StorSimpleClient.RestoreBackupAsync(deviceId, request);
+                          WriteObject(jobresult);
+                          WriteVerbose(String.Format("Your restore operation has been submitted for processing. Use commandlet \"Get-AzureStorSimpleJob -JobId {0}\" to track status.", jobresult.JobId));
+                      }
+                  });
+            }
+            catch (CloudException cloudException)
+            {
+                StorSimpleClient.ThrowCloudExceptionDetails(cloudException);
+            }
+        }
+
+        private void ProcessParameters()
+        {
+            var deviceInfos = StorSimpleClient.GetAllDevices(); //get list of all devices
+            foreach (var deviceInfo in deviceInfos)
+            {
+                if (deviceInfo.FriendlyName.Equals(DeviceName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    deviceId = deviceInfo.DeviceId;
+                }
+            }
+
+            if (deviceId == null)
+                throw new ArgumentException("Device with name " + DeviceName + "not found");
+            
+            finalBackupId = null;
+            finalSnapshotId = null;
+            switch (ParameterSetName)
+            {
+                case PARAMETERSET_BACKUPID:
+                    finalBackupId = BackupId.ToString();
+                    break;
+                case PARAMETERSET_BACKUPSNAPSHOTID:
+                    finalBackupId = BackupId.ToString();
+                    finalSnapshotId = SnapshotId;
+                    break;
+                case PARAMETERSET_BACKUPOBJECT:
+                    if (Backup == null)
+                        throw new ArgumentException("Specify valid object for Backup parameter");
+                    finalBackupId = Backup.InstanceId.ToString();
+                    break;
+                case PARAMETERSET_BACKUPSNAPSHOTOBJECT:
+                    if (Backup == null)
+                        throw new ArgumentException("Specify valid object for Backup parameter");
+                    if (Snapshot == null)
+                        throw new ArgumentException("Specify valid object for Snapshot parameter");
+                    finalBackupId = Backup.InstanceId.ToString();
+                    finalSnapshotId = SnapshotId;
+                    break;
+            }
+        }
+    }
+}
