20a56c3582d2fde1fe01942abc0bb0a2679c6387
0350e5c2bb454b476077ca4204900c57ec85c1d5 <madhur.tanwani@microsoft.com>
2014-12-03 00:04:38 +0530

    Support to read XML app.config configuration file

--- src/ServiceManagement/StorSimple/Commands.StorSimple.Test/ScenarioTests/Configuration/ConfigurationReader.ps1
+++ src/ServiceManagement/StorSimple/Commands.StorSimple.Test/ScenarioTests/Configuration/ConfigurationReader.ps1
@@ -14,6 +14,37 @@
 
 $global:StorSimpleGlobalConfigMap = $null
 
+function Read-XMLConfigFile($configFilePath)
+{
+	$methodName = "Read-ConfigFile"
+
+	Write-Verbose "$methodName : Reading configuration file '$configFilePath'"
+	if([string]::IsNullOrEmpty($configFilePath)) {
+		throw "$methodName : Config file path is not specified. Please specify file path OR set the environment variable STORSIMPLE_SDK_TEST_CONFIG_PATH."
+	}
+	
+	[xml] $configContent = Get-Content $configFilePath
+	if(!$configContent) {
+		throw "$methodName : Config file '$configFilePath' is either not readable or is empty"
+	}
+	Write-Verbose "$methodName : Configuration file read. Extracting configuration keys."
+
+	$configMap=@{}
+	foreach($configEntry in $configContent.configuration.appSettings.add) {
+		Write-Verbose ("{0} : {1}" -F $configEntry.key, $configEntry.Value)
+		if($configMap.ContainsKey($configEntry.key)) {
+			throw "Duplicate configuration entries for key '$($configEntry.key)' - please check the configuration file '$configFilePath' for correctness"
+		} else {
+			$configMap.Add($configEntry.key, $configEntry.Value)
+		}
+	}
+	$global:StorSimpleGlobalConfigMap = $configMap
+
+	Write-Verbose "$methodName : Completed reading configuration items from file '$configFilePath'. Found $($configMap.Count) keys : "
+	$configMap.GetEnumerator() | foreach-object -begin { $i = 1 } -process { Write-Verbose ("$methodName : Config entry #{0}: '{1}' : '{2}'" -F $i, $_.Key, $_.Value); $i++}
+	return $configMap
+}
+
 function Read-ConfigFile($configFilePath)
 {
 	$methodName = "Read-ConfigFile"
@@ -47,7 +78,7 @@ function Get-ConfigFileMap ()
 	}
 
 	$configFilePath = $env:STORSIMPLE_SDK_TEST_CONFIG_PATH
-	$configurationMap = Read-ConfigFile $configFilePath
+	$configurationMap = Read-XMLConfigFile $configFilePath
 
 	if(!$configurationMap -or $configurationMap.Count -eq 0) {
 		throw "$methodName : The configuration map does not contain any keys - please check the configuration file '$configFilePath' for correctness"
